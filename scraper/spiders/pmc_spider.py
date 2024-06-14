71bab819afbd3ce175126a11148a3e17f2ce2577
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-06-14 16:45:56 -0400

    updates

--- scraper/spiders/pmc_spider.py
+++ scraper/spiders/pmc_spider.py
@@ -5,13 +5,17 @@ import scrapy
 
 class NationalLibMedSpider(scrapy.Spider):
     name = "pmc"
-
     start_urls = ["https://www.ncbi.nlm.nih.gov/pmc/?term=kcnq5"]
+    base_url = 'https://www.ncbi.nlm.nih.gov'
 
     def parse(self, response):
         articles = response.css("div.rprt")
         for article in articles:
             yield {
                 "title": article.css("div.title a").xpath("string()").get(),
-                "authors": article.css("div.desc::text").get()
+                "authors": article.css("div.desc::text").get(),
+                "href": f'{self.base_url}{article.css(".title a::attr(href)").get()}'
             }
+# output file command
+# scrapy crawl pmc -O articles.json
+# scrapy crawl pmc -o articles.jsonl
