760ab686a74eed1c35a41ece01280b3c1de247bd
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-06-15 16:18:31 -0400

    changed selectors to work for pubmed vs pmc

--- scraper/spiders/pubmed.py
+++ scraper/spiders/pubmed.py
@@ -5,16 +5,23 @@ import scrapy
 
 class NationalLibMedSpider(scrapy.Spider):
     name = "pubmed"
-    start_urls = ["https://www.ncbi.nlm.nih.gov/pmc/?term=kcnq5"]
-    base_url = 'https://www.ncbi.nlm.nih.gov'
+    start_urls = ["https://pubmed.ncbi.nlm.nih.gov/?term=kcnq5"]
+    base_url = 'https://pubmed.ncbi.nlm.nih.gov'
 
     def parse(self, response):
-        articles = response.css("div.rprt")
+        articles = response.css("div.docsum-content")
         for article in articles:
+            citation = {
+                "short_authors": response.css(".short-authors::text").get(),
+                "full_authors": response.css(".full-authors::text").get(),
+                "journal": response.css(".short-journal-citation::text").get(),
+                "pmid": response.css(".docsum-pmid::text").get(),
+            }
             yield {
-                "title": article.css("div.title a").xpath("string()").get(),
-                "authors": article.css("div.desc::text").get(),
-                "href": f'{self.base_url}{article.css(".title a::attr(href)").get()}'
+                "title": article.css("a").xpath("string()").get().replace('\n', '').strip(),
+                "snippet": article.css(".short-view-snippet").xpath("string()").get().replace('\n', '').strip(),
+                "citation": f'{citation["short_authors"]} {citation["journal"]} PMID: {citation["pmid"]}',
+                "url": f'{self.base_url}{article.css("a::attr(href)").get()}'
             }
 # output file command
 # scrapy crawl pmc -O articles.json
